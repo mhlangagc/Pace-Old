2b534cef579fe8ed11d8911bc045a960b9565a76
Gugulethu Mhlanga <mhlangagc@gmail.com>
2017-03-05 17:02:14 +0200

    Some updates to the workout model.

--- Pace/ExploreViewModel.swift
+++ Pace/ExploreViewModel.swift
@@ -18,39 +18,8 @@ class ExploreViewModel : NSObject {
 		capeArgus.daysAway = "33 DAYS AWAY".uppercased()
 		capeArgus.eventImage = UIImage(named: "4")
 		
-		
 		return [capeArgus]
 		
 	}
 	
-	func suggestedTrainers() -> [UserModel] {
-		
-		let trainer_1 = UserModel()
-		trainer_1.name = "Maria Sharapova"
-		trainer_1.profileImage = UIImage(named: "maria")
-		trainer_1.location = "Nyagan, Russia"
-		trainer_1.trainerDescription = "Maria Yuryevna Sharapova is a Russian professional tennis player. A United States resident since 1994, Sharapova has competed on the WTA tour since 2001"
-		
-		let trainer_2 = UserModel()
-		trainer_2.name = "Ell Marshall"
-		trainer_2.profileImage = UIImage(named: "trainer")
-		trainer_2.location = "Dorset, U.K"
-		trainer_2.trainerDescription = "Lean Muscle Specialist"
-		
-		let trainer_3 = UserModel()
-		trainer_3.name = "Mila George"
-		trainer_3.profileImage = UIImage(named: "cynthia")
-		trainer_3.location = "South Shawna, U.S"
-		trainer_3.trainerDescription = "Physiotherapy, Pilates & Yoga Specialist"
-		
-		let trainer_4 = UserModel()
-		trainer_4.name = "Justin Margolis"
-		trainer_4.profileImage = UIImage(named: "justin")
-		trainer_4.location = "South Carolina, U.S"
-		trainer_4.trainerDescription = "X-Marine Specialising in Muscle Building and Endurance Training"
-		
-		
-		
-		return [trainer_1, trainer_2, trainer_3, trainer_4]
-	}
 }
