c4619fda494663dc1bbccafddc92874d51f89da9
Gugulethu Mhlanga <mhlangagc@gmail.com>
2017-03-12 18:50:57 +0200

    Downloaded workout now showing in the “All workouts section” of the routine view.

--- PaceAppServices.swift
+++ PaceAppServices.swift
@@ -237,6 +237,54 @@ class PaceAppServices : NSObject {
 	}
 	
 	
+	func retrieveUserDownloadedWorkouts(completion: @escaping (_ result: [ExploreWorkoutModel]) -> Void) {
+		
+		var workoutsArray = [ExploreWorkoutModel]()
+		let userID = FIRAuth.auth()!.currentUser!.uid
+		
+		UIApplication.shared.isNetworkActivityIndicatorVisible = true
+		
+		let fanUserDownloadedRef = FIRDatabase.database().reference().child("fan-User-PurchasedWorkouts").child(userID)
+		
+		fanUserDownloadedRef.observe(.childAdded, with: { (snapshot) in
+			
+			let workoutId = snapshot.key
+			
+			let workoutRef = FIRDatabase.database().reference().child("Workouts-Teams").child(workoutId)
+			workoutRef.observe(FIRDataEventType.value, with: { (snapShot) in
+				
+				if let dictionary = snapShot.value as? [String: AnyObject] {
+					
+					let featuredWorkout = ExploreWorkoutModel()
+					
+					featuredWorkout.workoutID = workoutId
+					featuredWorkout.name = dictionary["name"] as? String
+					featuredWorkout.workoutDescription = dictionary["workoutDescription"] as? String
+					featuredWorkout.backgroundImageUrl = dictionary["backgroundImageUrl"] as? String
+					featuredWorkout.time = dictionary["time"] as? Int
+					featuredWorkout.rating = dictionary["rating"] as? Int
+					featuredWorkout.numberOfReviews = dictionary["numberOfReviews"] as? Int
+					featuredWorkout.workoutPrice = (dictionary["workoutPrice"] as? Double).map { PriceEnum(rawValue: $0) }!
+					featuredWorkout.workoutCatergory = (dictionary["workoutCatergory"] as? String).map { WorkoutCatergory(rawValue: $0) }!
+					featuredWorkout.trainerID = dictionary["trainerID"] as? String
+					
+					workoutsArray.append(featuredWorkout)
+					
+					
+					completion(workoutsArray)
+					
+					
+				}
+				
+			}, withCancel: nil)
+			
+		}, withCancel: nil)
+		
+	}
+
+
+	
+	
 	func retrieveTrainer(exploreWorkout: ExploreWorkoutModel, completion: @escaping (_ result: User) -> Void) {
 		
 		if let trainerID = exploreWorkout.trainerID {
