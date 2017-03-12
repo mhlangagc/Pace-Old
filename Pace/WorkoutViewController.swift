13e346462711a9b2de1c2dbaf51feeec7e7895e3
Gugulethu Mhlanga <mhlangagc@gmail.com>
2017-03-12 23:44:36 +0200

    fixed a small bug causing a crash in viewing a trainer.

--- Pace/WorkoutViewController.swift
+++ Pace/WorkoutViewController.swift
@@ -42,22 +42,23 @@ class WorkoutViewController : UIViewController, UITableViewDataSource, UITableVi
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		paceAppService.retrieveUserDownloadedWorkoutIDs { (IDs) in
-			
-			self.downloadedWorkoutIDArray = IDs
-			self.checkIfWorkoutIsDownloaded()
-		}
+//		paceAppService.retrieveUserDownloadedWorkoutIDs { (IDs) in
+//			
+//			self.downloadedWorkoutIDArray = IDs
+//			self.checkIfWorkoutIsDownloaded()
+//		}
 		
 		self.setupWorkoutDetailsTableView()
 		self.setupNavigationBar()
+		self.setupGetButton()
 		view.backgroundColor = UIColor.black
 		workoutDetailsTableView?.register(ExerciseCellView.self, forCellReuseIdentifier: exerciseCellID)
 		
 		self.setupHeaderView()
 		
-		
-		
 		paceAppService.retrieveTrainer(exploreWorkout: exploreWorkout!) { (workoutTrainer) in
+			
+			self.trainer = workoutTrainer
 			
 			if let trainerName = workoutTrainer.name, let trainerImageUrl = workoutTrainer.profileImageUrl {
 				
@@ -79,7 +80,7 @@ class WorkoutViewController : UIViewController, UITableViewDataSource, UITableVi
 	
 	func setupWorkoutDetailsTableView() {
 		
-		let tableViewFrame = CGRect(x: 0.0, y: 0.0, width: view.frame.width, height: view.frame.height - 80.0)
+		let tableViewFrame = CGRect(x: 0.0, y: 0.0, width: view.frame.width, height: view.frame.height - 144.0)
 		workoutDetailsTableView = UITableView(frame: tableViewFrame, style: UITableViewStyle.plain)
 		workoutDetailsTableView?.backgroundColor = .black
 		workoutDetailsTableView?.delegate = self
@@ -171,7 +172,7 @@ class WorkoutViewController : UIViewController, UITableViewDataSource, UITableVi
 	
 	func setupGetButton() {
 		
-		getButtonView = GetButtonView.init(frame: CGRect(x: 0, y: view.frame.height - 80.0, width: view.frame.width, height: 80.0))
+		getButtonView = GetButtonView.init(frame: CGRect(x: 0, y: view.frame.height - 144.0, width: view.frame.width, height: 80.0))
 		getButtonView?.workoutDetailsVC = self
 		
 		let price = self.exploreWorkout?.workoutPrice?.rawValue
