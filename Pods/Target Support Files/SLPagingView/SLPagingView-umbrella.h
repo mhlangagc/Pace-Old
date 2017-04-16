7cab6f551b43be726718c2092eacd57f043e18dd
Gugulethu Mhlanga <mhlangagc@gmail.com>
2017-04-16 03:52:25 +0200

    Updated a number of outdate pods.

---
+++ Pods/Target Support Files/SLPagingView/SLPagingView-umbrella.h
@@ -0,0 +1,12 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "UIColor+SLAddition.h"
+#import "UIScrollView+UpdateContentSize.h"
+#import "ShareResources.h"
+#import "SLPagingViewController.h"
+
+FOUNDATION_EXPORT double SLPagingViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char SLPagingViewVersionString[];
+
